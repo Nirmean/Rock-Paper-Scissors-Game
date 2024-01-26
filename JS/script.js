12be40d37212f28e2d9e631a26f1fe1195e94e0b
Nirmean <ana_noona@live.com>
2024-01-26 13:47:36 +0000

    computer move function created, event listeners added to buttons

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
 // create buttons for user to play game with event listener
 
 // JQuery
-const rock = $("<button>").addClass("rock").text("Rock");
-const paper = $("<button>").addClass("paper").text("Paper");
-const scissors = $("<button>").addClass("scissors").text("Scissors");
+const rock = $("<button>").addClass("rock").text("Rock").on("click", playersMove('rock'));
+const paper = $("<button>").addClass("paper").text("Paper").on("click", playersMove('paper'));
+const scissors = $("<button>").addClass("scissors").text("Scissors").on("click", playersMove('scissors'));
 const reset = $("<button>").addClass("reset").text("Reset Score");
 const buttonContainer = $("<div>").addClass("btn-container");
 $(buttonContainer).append(rock, paper, scissors, reset);
@@ -21,16 +21,39 @@ $("body").append(buttonContainer);
 // document.body.appendChild(scissors);
 
 // function that creates the computers move
-    // variable that creates a random number
-    // empty variable that will store the future computer move
-    // if statements that assign a value of 'rock, paper, scissors'
-    // return the computers move
+
+function pickComputerMove() {
+     // variable that creates a random number
+    const randomNumber = Math.random();
+     // empty variable that will store the future computer move
+    let computerMove = '';
+     // if statements that assign a value of 'rock, paper, scissors'
+    if (randomNumber >= 0 && randomNumber <= 1 / 3) {
+        computerMove = 'rock';
+    } else if (randomNumber >= 1 / 3 && randomNumber <= 2 / 3) {
+        computerMove = 'paper';
+    } else if (randomNumber >= 2 / 3 && randomNumber <= 1) {
+        computerMove = 'scissors';
+    }
+     // return the computers move
+    return computerMove;
+}
 
 // playGame function that takes in player move and compares with computer
+
+function playersMove(playerMove) {
     // create a var that stores the computer move function
+    const computerMove = pickComputerMove();
     // empty variable for the result
+    let result = '';
+
     // if statements that compare player move with computer move (for all positions)
-    // alert results
+    if (playerMove === rock) {
+        result = 'tie';
+    }
+     // alert results   
+    alert(`You picked ${playerMove}. Computer picked ${computerMove}. ${result}.`);
+}
 
 
 // var that holds the score baseline
